ece47e28b4c4dbe679e8aba44b4772550bf396d6
gyumei <hannibal221b219@gmail.com>
2023-01-07 00:02:51 +0900

    deployのコミット

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Paginator::useBootstrap();  
         \URL::forceScheme('https');
         $this->app['request']->server->set('HTTPS','on');
