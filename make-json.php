9fc4247966630bfa2bf6898be272cfafea754c0e
gyumei <hannibal221b219@gmail.com>
2023-01-09 01:14:29 +0900

    修正

--- make-json.php
+++ make-json.php
@@ -17,7 +17,7 @@ class make_json {
             "client_x509_cert_url"=> env("client_x509_cert_url"),
         );
     // 連想配列($array)をJSONに変換(エンコード)する
-    $json = json_encode( $array ) ;
+    $json = json_encode( $array );
     return $json;
     }
 }
