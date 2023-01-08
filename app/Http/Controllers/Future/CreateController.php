58f99c3626761127a92ef7879ba4c5fc431536a1
gyumei <hannibal221b219@gmail.com>
2023-01-09 03:17:14 +0900

    修正

--- app/Http/Controllers/Future/CreateController.php
+++ app/Http/Controllers/Future/CreateController.php
@@ -126,8 +126,8 @@ class CreateController extends Controller
         //JSONファイルの指定
         $make_json = new Make_json();
         $json = $make_json->get_json();
-        file_put_contents(storage_path(made_json.json),$json);
-        $client->setAuthConfig(storage_path(made_json.json));
+        file_put_contents(made_json.json,$json);
+        $client->setAuthConfig(made_json.json);
 
         return $client;
     }
