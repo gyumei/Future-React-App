d4ea3521c470405ea6bd085737497ed5d209e13c
gyumei <hannibal221b219@gmail.com>
2023-01-09 12:49:12 +0900

    修正

--- app/Http/Controllers/Future/CreateController.php
+++ app/Http/Controllers/Future/CreateController.php
@@ -137,7 +137,7 @@ class CreateController extends Controller
             "client_x509_cert_url"=> env("client_x509_cert_url"),
         );
         // 連想配列($array)をJSONに変換(エンコード)する
-         $json = json_encode( $array ) ;
+
         $client->setAuthConfig($json);
 
         return $client;
