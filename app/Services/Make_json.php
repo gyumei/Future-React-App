d90e3564b5e792df162658ab46a369dd51e8d74c
gyumei <hannibal221b219@gmail.com>
2023-01-10 18:31:37 +0900

    修正

--- app/Services/Make_json.php
+++ app/Services/Make_json.php
@@ -18,7 +18,6 @@ class make_json{
         );
     // 連想配列($array)をJSONに変換(エンコード)する
     $json = json_encode( $array ) ;
-    dd($json);
     $filename = '/app/public/make_json.json';
     // ファイルを開く（'w'は書き込みモード）
     $number = file_put_contents($filename, $json);
